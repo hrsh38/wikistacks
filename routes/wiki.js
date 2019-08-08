ede70be106faf8c7ec8aef6a491a16a7622ebe00
Austin Trow <austintrow@gmail.com>
2019-08-08 13:01:09 -0400

    started post method

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const {addPage} = require('../views');
 const router = express.Router();
+const { Page } = require("../models");
 
 
 router.get('/', (req, res, next) => {
     res.send('GET /wiki/');
 });
 
-router.post('/', (req, res, next) => {
-    res.json(req.body);
+router.post('/', async (req, res, next) => {
+    const page = new Page({
+        title: req.body.title,
+        content: req.body.content
+    });
+    try {
+        await page.save();
+        res.redirect('/');
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get('/add', (req, res, next) => {
