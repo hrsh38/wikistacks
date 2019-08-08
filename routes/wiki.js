c6fcec29be44febcadb1b7872f8e92823b9af6c0
Harsh Patel <harshp97@gmail.com>
2019-08-08 16:42:05 -0400

    Mainpage

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const {addPage} = require('../views');
+const {addPage, wikiPage, main} = require('../views');
 const router = express.Router();
 const { Page } = require("../models");
 const slugify = require('slugify');
 
 
-router.get('/', (req, res, next) => {
-    res.send('GET /wiki/');
+router.get('/', async (req, res, next) => {
+    const pages = await Page.findAll();
+    res.send(main(pages));
 });
 
 router.post('/', async (req, res, next) => {
@@ -20,7 +21,7 @@ router.post('/', async (req, res, next) => {
     });
     try {
         await page.save();
-        res.redirect(`/wiki/${page.id}`);
+        res.redirect(`/wiki/${page.slug}`);
     } catch (error) {
         next(error);
     }
@@ -28,6 +29,14 @@ router.post('/', async (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
     res.send(addPage());
+});
+
+router.get('/:slug', async (req, res, next) => {
+    const page = await Page.findOne({
+        where: {slug: req.params.slug}
+    });
+    //console.log(page);
+    res.send(wikiPage(page, page.author));
 });
 
 module.exports = router;
